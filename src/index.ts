0d2505bfde426544291ddb4a962bfa4930960773
Chris <chrisbreuer93@gmail.com>
2024-10-19 22:59:50 +0200

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'node:path'
+import { existsSync } from 'node:fs'
+import { deepMerge } from './utils'
+
+export interface ConfigOptions<T> {
+  name: string
+  cwd?: string
+  defaultConfig: T
+}
+
+export async function loadConfig<T extends Record<string, unknown>>({ name, cwd, defaultConfig }: ConfigOptions<T>): Promise<T> {
+  const c = cwd ?? process.cwd()
+  const configPath = resolve(c, `${name}.config`)
+
+  if (existsSync(configPath)) {
+    try {
+      const importedConfig = await import(configPath)
+      const loadedConfig = importedConfig.default || importedConfig
+      return deepMerge(defaultConfig, loadedConfig)
+    }
+    catch (error) {
+      console.error(`Error loading config from ${configPath}:`, error)
+    }
+  }
+
+  return defaultConfig
+}
