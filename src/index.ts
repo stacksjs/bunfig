0acd13a7982296713e2f25e3edda59e4391c78d8
Chris <chrisbreuer93@gmail.com>
2025-01-08 15:23:28 +0100

    chore: add missing docblock option

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { deepMerge } from './utils'
  * @param {string} options.name - The name of the configuration file.
  * @param {string} [options.cwd] - The current working directory.
  * @param {string} [options.endpoint] - The API endpoint to fetch config from in browser environments.
+ * @param {string} [options.headers] - The headers to send with the request in browser environments.
  * @param {T} options.defaultConfig - The default configuration.
  * @returns {Promise<T>} The merged configuration.
  * @example ```ts
