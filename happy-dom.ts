d15fd77f36f6b0e83426504c89cad8f5a6aee96d
glennmichael123 <gtorregosa@gmail.com>
2026-04-27 21:48:37 +0800

    fix: suppress lint rules for very-happy-dom workaround import

--- happy-dom.ts
+++ happy-dom.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line pickier/no-import-dist, pickier/no-import-node-modules-by-path
 import { GlobalRegistrator } from './node_modules/very-happy-dom/dist/index.js'
 
 GlobalRegistrator.register()
